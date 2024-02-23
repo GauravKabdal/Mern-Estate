0bc42bc85b2c52bf728d5126621e47e969a3c4f1
Gaurav Kabdal <gauravkabdal10@gmail.com>
2024-02-23 15:05:17 +0530

    Create user update api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,6 +7,7 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import profileBg from "../assets/profile_background.jpg";
 
 const Profile = () => {
   const [filePerc, setFilePerc] = useState(0);
@@ -47,63 +48,69 @@ const Profile = () => {
   };
 
   return (
-    <div className="flex flex-col p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
-        <input
-          type="file"
-          name=""
-          id=""
-          ref={fileRef}
-          hidden
-          accept="image/*"
-          onChange={(e) => setFile(e.target.files[0])}
-        />
-        <img
-          onClick={() => fileRef.current.click()}
-          src={formData.avatar || currentUser.avatar}
-          alt=""
-          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
-        />
-        <p className="text-sm self-center">
-          {fileUploadError ? (
-            <span className="text-red-700">
-              Error uploading Image(image should be less than 2MB)
+    <div className="bg-cover bg-center bg-no-repeat bg-[url('./assets/profile_background.jpg')]">
+      <div className="flex flex-col p-3 max-w-lg mx-auto ">
+        <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+        <form className="flex flex-col gap-4">
+          <input
+            type="file"
+            name=""
+            id=""
+            ref={fileRef}
+            hidden
+            accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <img
+            onClick={() => fileRef.current.click()}
+            src={formData.avatar || currentUser.avatar}
+            alt=""
+            className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+          />
+          <p className="text-sm self-center">
+            {fileUploadError ? (
+              <span className="text-red-700">
+                Error uploading Image(image should be less than 2MB)
+              </span>
+            ) : filePerc > 0 && filePerc < 100 ? (
+              <span className="text-slate-700">{`Uploading ${filePerc} %`}</span>
+            ) : filePerc == 100 ? (
+              <span className="text-green-700">Uploaded Successfully</span>
+            ) : (
+              " "
+            )}
+          </p>
+          <input
+            type="text"
+            placeholder="username"
+            className="border p-3 rounded-lg"
+            id="username"
+          />
+          <input
+            type="text"
+            placeholder="email"
+            className="border p-3 rounded-lg"
+            id="email"
+          />
+          <input
+            type="text"
+            placeholder="password"
+            className="border p-3 rounded-lg"
+            id="password"
+          />
+          <button className="transition ease-in duration-500 bg-slate-700 font-bold text-white rounded-lg p-3 uppercase hover:text-slate-700 hover:bg-slate-300 disabled:opacity-70">
+            Update
+          </button>
+          <div className="flex justify-between mt-1">
+            <span className="transition ease-in duration-500 text-red-700 cursor-pointer bg-white rounded p-3 font-bold hover:bg-red-700 hover:text-white">
+              Delete Account
             </span>
-          ) : filePerc > 0 && filePerc < 100 ? (
-            <span className="text-slate-700">{`Uploading ${filePerc} %`}</span>
-          ) : filePerc == 100 ? (
-            <span className="text-green-700">Uploaded Successfully</span>
-          ) : (
-            " "
-          )}
-        </p>
-        <input
-          type="text"
-          placeholder="username"
-          className="border p-3 rounded-lg"
-          id="username"
-        />
-        <input
-          type="text"
-          placeholder="email"
-          className="border p-3 rounded-lg"
-          id="email"
-        />
-        <input
-          type="text"
-          placeholder="password"
-          className="border p-3 rounded-lg"
-          id="password"
-        />
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-70">
-          Update
-        </button>
-        <div className="flex justify-between mt-5">
-          <span className="text-red-700 cursor-pointer">Delete Account</span>
-          <span className="text-red-700 cursor-pointer">Sign Out</span>
-        </div>
-      </form>
+            <span className="transition ease-in duration-500 text-red-700 cursor-pointer  bg-white rounded p-3 font-bold hover:bg-red-700 hover:text-white">
+              Sign Out
+            </span>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
