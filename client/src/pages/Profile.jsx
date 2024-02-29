7e2b5ad7c6bfff8c3ff06b232096a79c995e3b5f
Gaurav Kabdal <gauravkabdal10@gmail.com>
2024-02-29 14:55:56 +0530

    complete update uer functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useRef, useState, useEffect } from "react";
 import {
   getDownloadURL,
@@ -6,15 +6,23 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
+
 import { app } from "../firebase";
-import profileBg from "../assets/profile_background.jpg";
+
+import {
+  updateUserStart,
+  updateUserFailure,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
 
 const Profile = () => {
+  const dispatch = useDispatch();
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setFile] = useState(undefined);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
   const [formData, setFormData] = useState({});
 
   useEffect(() => {
@@ -47,11 +55,46 @@ const Profile = () => {
     );
   };
 
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    console.log(formData);
+  };
+
+  const handleSubmit = async (e) => {
+    console.log("handle Submit reached !");
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      console.log("reached update start");
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      console.log(res);
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        console.log("reached data update failure!");
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="bg-cover bg-center bg-no-repeat bg-[url('./assets/profile_background.jpg')]">
       <div className="flex flex-col p-3 max-w-lg mx-auto ">
         <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <input
             type="file"
             name=""
@@ -64,7 +107,7 @@ const Profile = () => {
           <img
             onClick={() => fileRef.current.click()}
             src={formData.avatar || currentUser.avatar}
-            alt=""
+            alt="Profile Photo"
             className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
           />
           <p className="text-sm self-center">
@@ -83,23 +126,30 @@ const Profile = () => {
           <input
             type="text"
             placeholder="username"
+            defaultValue={currentUser.username}
             className="border p-3 rounded-lg"
             id="username"
+            onChange={handleChange}
           />
           <input
             type="text"
+            defaultValue={currentUser.email}
             placeholder="email"
             className="border p-3 rounded-lg"
             id="email"
+            onChange={handleChange}
           />
           <input
-            type="text"
+            type="password"
             placeholder="password"
             className="border p-3 rounded-lg"
             id="password"
           />
-          <button className="transition ease-in duration-500 bg-slate-700 font-bold text-white rounded-lg p-3 uppercase hover:text-slate-700 hover:bg-slate-300 disabled:opacity-70">
-            Update
+          <button
+            disabled={loading}
+            className="transition ease-in duration-500 bg-slate-700 font-bold text-white rounded-lg p-3 uppercase hover:text-slate-700 hover:bg-slate-300 disabled:opacity-70"
+          >
+            {loading ? "Loading..." : "Update"}
           </button>
           <div className="flex justify-between mt-1">
             <span className="transition ease-in duration-500 text-red-700 cursor-pointer bg-white rounded p-3 font-bold hover:bg-red-700 hover:text-white">
@@ -109,6 +159,10 @@ const Profile = () => {
               Sign Out
             </span>
           </div>
+          {error && <p className="text-red-700 mt-3">{error}</p>}
+          {updateSuccess && (
+            <p className="text-green-700">Updated Successfully!!</p>
+          )}
         </form>
       </div>
     </div>
